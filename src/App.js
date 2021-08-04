b7ca8046d8411b36b93685701b05e75dd37cd624
Ayo <bimboladxtreme@gmail.com>
2021-08-04 19:43:10 +0100

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
 
 function App() {
-	return <div className="App">HELLO WORLD </div>;
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+	return (
+		<div className="home">
+			THE HOMEPAGE
+			<div className="home__header"></div>
+			<div className="home__body"></div>
+		</div>
+	);
+}
+
+export default Home;
